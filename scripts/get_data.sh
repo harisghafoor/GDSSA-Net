004316b80baaa0bb965d78bfb93ab910f40dfa74
Haris Ghafoor <70271396+harisghafoor@users.noreply.github.com>
2024-01-16 10:05:29 +0900

    Update get_data.sh

--- scripts/get_data.sh
+++ scripts/get_data.sh
@@ -11,13 +11,10 @@ echo "Downloading TN3K dataset..."
 curl -L -o "$tn3k_zip" "$tn3k_url" && unzip -q "$tn3k_zip" -d "$tn3k_dir" && rm "$tn3k_zip"
 
 # Download/unzip DDTI dataset (assuming it's another Google Drive link)
-ddti_url="YOUR_DDTI_DATASET_URL_HERE"
+ddti_url="YOUR_DDTI_https://drive.google.com/uc?id=1reHyY5eTZ5uePXMVMzFOq5j3eFOSp50FDATASET_URL_HERE"
 ddti_zip="./datasets/ddti_dataset.zip"
 ddti_dir="./datasets/ddti_dataset"
 echo "Downloading DDTI dataset..."
 curl -L -o "$ddti_zip" "$ddti_url" && unzip -q "$ddti_zip" -d "$ddti_dir" && rm "$ddti_zip"
-
-# Rest of your script for COCO dataset (if needed)
-# ...
 
 echo "All datasets downloaded and unzipped."
